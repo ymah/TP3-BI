ab83a7b9910d55b6fe819f0bc9075adab20c8ecd
Yaker Mahieddine <yaker@a13p2.fil.univ-lille1.fr>
2015-02-23 15:47:56 +0100

    changement de source en src

---
+++ TP_a_completer/src/clustering/DistanceEuclidienne.java
@@ -0,0 +1,13 @@
+package clustering;
+
+public class DistanceEuclidienne implements Distance {
+
+	@Override
+	public double valeur(Donnee d1, Donnee d2) {
+		
+		for(int i = 0;i<d1.getDim();i++){
+			
+		}
+	}
+
+}
